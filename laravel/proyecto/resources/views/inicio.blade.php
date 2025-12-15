9f922c32eff98fb807211871c5788106c62720cc
TorreMiguel <mlmiguel307@gmail.com>
2025-12-15 10:56:57 +0100

    quito proyecto de laravel

--- laravel/proyecto/resources/views/inicio.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="text-center">
-        <h1>¡Bienvenido a Laravel!</h1>
-        <p>Esta es la página de inicio usando plantillas Blade.</p>
-    </div>
-@endsection
